37b93c169afcf89c0471d15fbe03be0eb41e95de
xiongyingcheng <@ding_34066>
2021-10-28 16:10:42 +0800

    水果成篮

--- 数组/长度最小的子数组(滑动窗口).js
+++ 数组/长度最小的子数组(滑动窗口).js
@@ -6,28 +6,44 @@
 // 双循环一直寻找符合条件的数组
 function solution1(val, nums) {
   const len = nums.length;
-  let currentArr = [];
-  let result = [];
+  // 初始化为最大值，能够比较
+  let result = Infinity;
   let subLen = 0;
+  let sum = 0;
   for (let i = 0; i < len; i++) {
-    let sum = 0;
+    sum = 0
     for (let j = i; j < len; j++) {
       sum += nums[j];
-      currentArr.push(nums[j]);
       // 遇到总和大于等于目标值时，立刻停止，记录当前子数组，取最短的数组，然后继续下一次循环
       if (sum >= val) {
         subLen = j - i + 1;
-        result = result.length === 0 ? currentArr :
-          result.length > currentArr.length ? currentArr : result;
-        currentArr = [];
+        result = result < subLen ? result : subLen;
         break;
       }
     }
   }
   return result
 }
-console.log(solution1(7, [2, 3, 1, 2, 4, 3]));
+// console.log(solution1(7, [2, 3, 1, 2, 4, 3]));
 
 // 2.滑动窗口（双指针的一种） O(n)
 // 演示动画https://code-thinking.cdn.bcebos.com/gifs/209.%E9%95%BF%E5%BA%A6%E6%9C%80%E5%B0%8F%E7%9A%84%E5%AD%90%E6%95%B0%E7%BB%84.gif
 // 
+function solution2(val, nums) {
+  let start = 0;
+  let subLen = 0;
+  let result = Infinity;
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    // 这里用while是为了把多出val的值减干净
+    while (sum >= val) {
+      subLen = i - start + 1;
+      // 滑动窗口的精髓在这里，每次值大了，就移动左边窗口，值小了就移动右边窗口
+      sum -= nums[start++];
+      result = result < subLen ? result : subLen
+    }
+  }
+  return result
+}
+console.log(solution2(7, [2, 3, 1, 2, 4, 3]));
